57b88223ce570a504ee18e0bbe7e0a28a2cf6c10
Thomas <anhnt@avada.io>
2022-08-05 10:00:24 +0700

    Change route list to array for easier splitting

--- packages/assets/src/routes/routes.js
+++ packages/assets/src/routes/routes.js
@@ -4,10 +4,14 @@ import Home from '../loadables/Home';
 import Samples from '../loadables/Samples';
 import NotFound from '../loadables/NotFound';
 
+const routeGroups = [
+  {path: '/', component: Home},
+  {path: '/samples', component: Samples}
+];
+
 const Routes = () => (
   <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/samples" component={Samples} />
+    {routeGroups.map(group => group.map((props, key) => <Route key={key} exact {...props} />))}
     <Route path="*" component={NotFound} />
   </Switch>
 );
